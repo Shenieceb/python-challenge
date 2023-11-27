22d5d1ba045f211e3d1099ec38a2395ed3755c9f
Shenieceb <147777239+Shenieceb@users.noreply.github.com>
2023-11-27 17:29:10 -0500

    Update pybank.py
    Added code to compute total net profit loss

--- Python-Challenge-Assignment/PyBank/pybank.py
+++ Python-Challenge-Assignment/PyBank/pybank.py
@@ -11,16 +11,26 @@ file_to_load= r"C:\Users\glamh\OneDrive\Desktop\Python-Challenge-Assignment\PyBa
 
 
 count_of_months=0
+#starting_balance=0
+total_profit_loss=0
 with open(file_to_load, ) as csvfile:
 
     bank_data = csv.DictReader(csvfile)
     for row in bank_data:
         #print(row)
+        #print(row['Date'], ' ', row['Profit/Losses'])
+        #date=row['Date']
+        #print(row['Profit/Losses'])
         count_of_months+=1
+        starting_balance=int(row['Profit/Losses'])
+       # print(starting_balance)
+        total_profit_loss +=  starting_balance
 
-
-
+        #print(total_profit_loss)
         #print (count_of_months)
 print (count_of_months)
+print(total_profit_loss)
+
+ 
 
 
