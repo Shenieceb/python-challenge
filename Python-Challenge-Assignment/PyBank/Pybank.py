ac35ad1f6b212ff6603c4638656d81cc45b72d1b
Shenieceb <147777239+Shenieceb@users.noreply.github.com>
2024-06-26 12:58:54 -0400

    Add files via upload

--- Python-Challenge-Assignment/PyBank/Pybank.py
+++ Python-Challenge-Assignment/PyBank/Pybank.py
@@ -1,56 +1,54 @@
-import os
 import csv
+import os
+# Files to load and output (Remember to change these)
+#file_to_load = "Resources\budget_data.csv"
+#file_to_output = "analysis\analysis.txt"
 
-budget_data_csv = os.path.join("Resources","budget_data.csv")
+file_to_load= r"Python-Challenge-Assignment\PyBank\Resources\budget_data.csv"
+file_to_output= r"Python-Challenge-Assignment\PyBank\analysis\budget_analysis.txt"
 
-total_profit_loss = 0
-total_months = 0
-previous_profit_losses = None
-total_profit_losses_change = 0
-max_profit_increase = float('-inf')
-max_profit_increase_date = ""
-min_profit_decrease = float('inf')
-min_profit_decrease_date = ""
+count_of_months=0
+month_of_change=[]
+netchange_list=[]
+greatest_increase=["",0]
+greatest_decrease=["",9999999999999999999]
+total_profit_loss=0
+with open(file_to_load) as csvfile:
 
-# Open and read csv
-with open(budget_data_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
+    #print(header)
+    first_row=next(csvreader)
+    #print(first_row)
+    count_of_months+=1
+    total_profit_loss=int(first_row[1])
 
-    # Read the header row first
-    csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
+    prev_net=int(first_row[1])
+    #print(prev_net)
+    for row in csvreader:
+        count_of_months+=1
+        total_profit_loss+=int(row[1])
+        net_change=int(row[1])-prev_net
+        prev_net=int(row[1])
+        netchange_list.append(net_change)
+        month_of_change.append(row[0])
+        if net_change>greatest_increase[1]:
+            greatest_increase[0]=row[0]
+            greatest_increase[1]=net_change
+        if net_change<greatest_decrease[1]:
+            greatest_decrease[0]=row[0]
+            greatest_decrease[1]=net_change
 
-    # Count the total number of months included in the dataset
-
-    for row in csv_reader:
-        total_months += 1
-
-    #Sum the net total amount of "Profit/Losses" over the entire period
-        profit_losses = int(row[1])
-        total_profit_loss += profit_losses
-            
-    # The changes in "Profit/Losses" over the entire period, and then the average of those changes
-        if previous_profit_losses is not None:
-            profit_losses_change = profit_losses - previous_profit_losses
-            total_profit_losses_change += profit_losses_change
-            
-    # The greatest increase in profits (date and amount) over the entire period
-            if profit_losses_change > max_profit_increase:
-                max_profit_increase = profit_losses_change
-                max_profit_increase_date = row[0]
-
-    # The greatest decrease in profits (date and amount) over the entire period
-                if profit_losses_change < min_profit_decrease:
-                    min_profit_decrease = profit_losses_change
-                    min_profit_decrease_date = row[0]
-             
-        previous_profit_losses = profit_losses
-    average_profit_losses_change = round((total_profit_losses_change / (total_months - 1)),2)
-
-print("Financial Analysis")
-print("----------------------------")
-print("Total Months: " ,total_months)
-print("Total:", total_profit_loss)
-print("Average Change: $", average_profit_losses_change)
-print("Greatest Increase in Profits:", max_profit_increase_date,"($",max_profit_increase,")")
-print("Greatest Decrease in Profits:", min_profit_decrease_date,"($",min_profit_decrease,")")
+average_change=sum(netchange_list)/len(netchange_list)
+output=(
+    f"Financial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {count_of_months}\n"
+    f"Total: ${total_profit_loss}\n"
+    f"Average  Change: ${average_change:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n"
+)
+print(output)
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(output)
